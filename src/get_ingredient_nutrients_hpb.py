d882ef67450da133dde13d4cc8e543b11e2f632c
Chen Ning <chenningg97@gmail.com>
2020-12-08 14:55:58 +0800

    Edited scrapers

---
+++ src/get_ingredient_nutrients_hpb.py
@@ -0,0 +1,52 @@
+import csv
+
+# Extract ingredients by searching for ", raw" regex
+with open("./results/food_data_hpb_v2.csv", "r", newline="") as food_data_file:  # Input
+    with open(
+        "./results/ingredient_data_hpb_v1.csv", "a", newline=""
+    ) as food_ingredient_file:  # Output
+        food_data_reader = csv.reader(food_data_file, delimiter="|", quotechar="\\")
+
+        # Skip the headers row
+        next(food_data_reader, None)
+
+        spamwriter = csv.writer(
+            food_ingredient_file,
+            delimiter="|",
+            quotechar="\\",
+            quoting=csv.QUOTE_MINIMAL,
+        )
+
+        spamwriter.writerow(
+            [
+                "ingredient_name",
+                "serving_size",
+                "serving_size_unit",
+                "description",
+                "energy",
+                "energy_unit",
+                "protein",
+                "protein_unit",
+                "total_fat",
+                "total_fat_unit",
+                "saturated_fat",
+                "saturated_fat_unit",
+                "dietary_fibre",
+                "dietary_fibre_unit",
+                "carbohydrate",
+                "carbohydrate_unit",
+                "cholesterol",
+                "cholesterol_unit",
+                "sodium",
+                "sodium_unit",
+            ]
+        )
+
+        # Ignore headers
+        for row in food_data_reader:
+            # Check if this is an ingredient
+            food_name = row[0]
+
+            # If ingredient, we write it to output
+            if ", raw" in food_name:
+                spamwriter.writerow(row)
--- src/scrape_nutrition_info_hpb.py
+++ src/scrape_nutrition_info_hpb.py
@@ -15,7 +15,7 @@ load_dotenv()
 # Load config
 DRIVER_PATH = os.getenv("DRIVER_PATH")
 
-# Open Edge driver
+# Open Edge driver and set various settings
 options = EdgeOptions()
 options.use_chromium = True
 options.add_argument("headless")
@@ -24,20 +24,45 @@ driver = Edge(options=options, executable_path=DRIVER_PATH)
 
 food_names = []
 
+# Load in all food names to iterate through
 with open("./data/food_names_hpb.txt", "r") as food_names_file:
     for food_name in food_names_file:
         food_names.append(food_name.rstrip())
 
-with open("./results/food_data_hpb.csv", "a", newline="") as food_desc_file:
-    spamwriter = csv.writer(food_desc_file, delimiter='|',
-                            quotechar='\\', quoting=csv.QUOTE_MINIMAL)
+# Open new file to write results to
+with open("./results/food_data_hpb_v2.csv", "a", newline="") as food_desc_file:
+    spamwriter = csv.writer(
+        food_desc_file, delimiter="|", quotechar="\\", quoting=csv.QUOTE_MINIMAL
+    )
 
     # Go to get the nutritional info of all food in a loop
     driver.get("https://focos.hpb.gov.sg/eservices/ENCF/foodsearch.aspx")
 
     # Write headers to csv
-    spamwriter.writerow(["food_name", "description", "energy", "protein", "total_fat",
-                         "saturated_fat", "dietary_fibre", "carbohydrate", "cholesterol", "sodium"])
+    spamwriter.writerow(
+        [
+            "food_name",
+            "serving_size",
+            "serving_size_unit",
+            "description",
+            "energy",
+            "energy_unit",
+            "protein",
+            "protein_unit",
+            "total_fat",
+            "total_fat_unit",
+            "saturated_fat",
+            "saturated_fat_unit",
+            "dietary_fibre",
+            "dietary_fibre_unit",
+            "carbohydrate",
+            "carbohydrate_unit",
+            "cholesterol",
+            "cholesterol_unit",
+            "sodium",
+            "sodium_unit",
+        ]
+    )
 
     for food_name in food_names:
         # Remove trailing newlines
@@ -45,7 +70,8 @@ with open("./results/food_data_hpb.csv", "a", newline="") as food_desc_file:
 
         try:
             search_food_name = WebDriverWait(driver, 10).until(
-                EC.visibility_of_element_located((By.ID, "txtFoodName")))
+                EC.visibility_of_element_located((By.ID, "txtFoodName"))
+            )
 
             # Input food name
             search_food_name.clear()
@@ -54,9 +80,18 @@ with open("./results/food_data_hpb.csv", "a", newline="") as food_desc_file:
             # Search for the food
             driver.find_element_by_id("btnSearch").click()
 
+            # Get serving size of food
+            serving_size_element = driver.find_element_by_xpath(
+                "//*[@id='gvData']/tbody/tr[2]/td[3]"
+            )
+            serving_size_raw = serving_size_element.get_attribute("innerText").split()[
+                -1
+            ]
+            serving_size = serving_size_raw[1:-2]
+            serving_size_unit = serving_size_raw[-2]
+
             # Select the first checkbox of the food
-            driver.find_element_by_xpath(
-                "(//input[@type='checkbox'])[1]").click()
+            driver.find_element_by_xpath("(//input[@type='checkbox'])[1]").click()
 
             # Click confirm to load nutrition
             driver.find_element_by_id("btnConfirm").click()
@@ -65,29 +100,46 @@ with open("./results/food_data_hpb.csv", "a", newline="") as food_desc_file:
             description = ""
             if "Description:" in driver.page_source:
                 description = driver.find_element_by_xpath(
-                    "//*[@id='lblTable']/table/tbody/tr[2]/td[2]").get_attribute("innerText")
-                if (description == "nil" or description == "--"):
+                    "//*[@id='lblTable']/table/tbody/tr[2]/td[2]"
+                ).get_attribute("innerText")
+                if (
+                    description == "nil"
+                    or description == "Nil"
+                    or description == "--"
+                    or description == "-"
+                    or description == "na"
+                ):
                     description = ""
 
             # Create output
-            output_row = [food_name, description]
+            output_row = [food_name, serving_size, serving_size_unit, description]
 
             # Get nutritional data of food
             nutrition_table = driver.find_elements_by_xpath(
-                "((//*[@id='lblTable']/table/tbody/tr)[last()])/td/table/tbody/tr")
+                "((//*[@id='lblTable']/table/tbody/tr)[last()])/td/table/tbody/tr"
+            )
 
-            # Skip the first row (food name)
+            # Skip the first row (food name) loop and get all nutrients and their unit sizes
             for nutrient in nutrition_table[1:]:
-                amount = nutrient.find_element_by_xpath(
-                    ".//td[2]").get_attribute("innerText")
-                output_row.append(amount)
+                amount = (
+                    nutrient.find_element_by_xpath(".//td[2]")
+                    .get_attribute("innerText")
+                    .split()
+                )
+
+                if len(amount) == 0 or amount[0] == "NA":
+                    output_row.append("")
+                    output_row.append("")
+                else:
+                    # Append nutrient amount and unit of nutrient separately
+                    output_row.append(amount[0])
+                    output_row.append(amount[1])
 
             spamwriter.writerow(output_row)
 
             WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.ID, "btnBack"))).click()
-
-            # driver.find_element_by_id("btnBack").click()
+                EC.element_to_be_clickable((By.ID, "btnBack"))
+            ).click()
 
         # If we reach here, it means no results found for the food name search (e.g. 2pc chicken)
         except NoSuchElementException:
